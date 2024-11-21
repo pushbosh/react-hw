2a73aa7c4e718c9f0d2637d40f790fc69ae54994
pushbosh <aitaliev_r@auca.kg>
2024-11-21 10:57:07 +0600

    Finally finished

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import 'D:/---save---/Desktop/DZ7/react-hw/src/components/ProductCard/App.css';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
